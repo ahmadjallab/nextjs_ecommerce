a57c3b9a5e58cd656c5e54d51725c21b7fe9ead7
ahmadjamaljallab <ahm0189567@ju.edu.jo>
2024-06-10 19:36:10 +0300

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {  env: {
+        'ADMIN_USERNAME':'admin',
+        'HASHED_ADMIN_PASSWORD':'bPfwz6mY8GZVGcpCqxmcEoHCTjoXGXHJjT6+4gylvc468PNYHRMd/6h7aJ9lcfA90mcTzvDJHm1ChBcMRmWjcg=='
+    },};
+
+export default nextConfig;
